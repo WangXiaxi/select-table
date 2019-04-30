95ff9a0e363286b620cb51c04d6a9a8d87f0f21a
wangxx <995107408@qq.com>
2019-04-30 14:41:12 +0800

    update

--- src/plugin/components/g6-base/index.vue
+++ src/plugin/components/g6-base/index.vue
@@ -61,6 +61,48 @@ export default {
     /**
      * 更新当前选中的节点
      * @param model 需要更新的数据模型 object
+     * { // 节点
+     *  id: 'node1', // id 必须唯一
+     *  color: '#333', // 颜色（该颜色被认为是 ant-design-palettes 的6阶色，激
+     *  活颜色、选中会根据该色值自动设置）
+     *  size: 10 || [10, 10], // 尺寸 || [宽, 高]
+     *  shape: 'circle', // 所用图形
+     *  style: { // 关键形样式（优先级高于color）
+     *  fill: 'red',
+     *  stroke: 'blue'
+     *  },
+     *  label: '文本标签' || { // 文本标签 || 文本图形配置
+     *  text: '文本标签',
+     *  fill: 'green'
+     *  },
+     *  parent: 'group1', // 所属组
+     *  index: 1, // 渲染层级
+     * }
+     * { // 边
+     *   id: 'edge1', // id 必须唯一
+     *   source: 'node1', // 源节点 id
+     *   target: 'node2', // 目标节点 id
+     *   controlPoints: [{ // 控制点
+     *     x: 10,
+     *     y: 10
+     *   }],
+     *   sourceAnchor: 0, // 源节点锚点
+     *   targetAnchor: 2, // 目标节点锚点
+     *   shape: 'line', // 所用图形
+     *   style: { // 关键形样式（优先级高于color）
+     *     fill: 'red',
+     *     stroke: 'blue'
+     *   },
+     *   label: '文本标签' || { // 文本标签 || 文本图形配置
+     *     text: '文本标签',
+     *     fill: 'green'
+     *   },
+     *   labelRectStyle: { // 文本矩形底的样式
+     *     fill: 'blue'
+     *   },
+     *   parent: 'group1', // 所属组
+     *   index: 1, // 渲染层级
+     * }
      */
     updateGraph(model) {
       const editor = this.editor
